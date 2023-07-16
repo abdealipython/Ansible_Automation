f22295ce6858c91fbdc3d4bf98cb571257aa7210
abdealipython <abdealipython@gmail.com>
2023-07-16 03:32:40 -0400

    Ansible Automation Project

---
+++ user_creation.py
@@ -0,0 +1,21 @@
+---
+- hosts: localhost
+  gather_facts: no
+  tasks:
+    - name: Create 'three' group
+      group:
+        name: three
+        state: present
+
+    - name: Create 'other' group
+      group:
+        name: other
+        state: present
+
+    - name: Create user accounts
+      user:
+        name: "harman{{ item }}"
+        groups: "{{ 'three' if (item|int % 3) == 0 else 'other' }}"
+        state: present
+      loop: "{{ range(1, 16) | list }}"
+
